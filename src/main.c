e35d82f54e67c764d51688b847cd0dad7d25e944
Stef van Stipdonk <stefvanstipdonk@gmail.com>
2024-03-19 12:15:19 +0100

    Add basic file reading

--- src/main.c
+++ src/main.c
@@ -12,6 +12,9 @@
 
 void init(arena_t *arena);
 void execute_rule();
+void move_cells();
+void render_state();
+void parse_rule_file();
 
 typedef struct {
   SDL_Window *window;
@@ -21,7 +24,10 @@ typedef struct {
   bool cell_states[CELLSINHEIGHT][CELLSINWIDTH];
 
   // First entry is arr 2 is state, and 2nd is outcome.
-  bool rules[100][2][3];
+  struct {
+    bool rule[3];
+    bool center_outcome;
+  } rule_definitions[100];
   int rules_size;
 } state_t;
 
@@ -43,8 +49,8 @@ void init(arena_t *arena) {
                                    SDL_WINDOWPOS_CENTERED_DISPLAY(1),
                                    WINDOWWIDTH, WINDOWHEIGHT, SDL_WINDOW_SHOWN);
 
-  state->renderer =
-      SDL_CreateRenderer(state->window, -1, SDL_RENDERER_ACCELERATED);
+  state->renderer = SDL_CreateRenderer(
+      state->window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
   SDL_SetRenderDrawColor(state->renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
   SDL_RenderClear(state->renderer);
   SDL_RenderPresent(state->renderer);
@@ -57,26 +63,64 @@ void init(arena_t *arena) {
       state->cells[y][x]->y = y * CELLHEIGHT;
       state->cells[y][x]->h = CELLHEIGHT;
       state->cells[y][x]->w = CELLWIDTH;
-      state->cell_states[y][x] = false;
+      state->cell_states[y][x] = true;
       SDL_RenderFillRect(state->renderer, state->cells[y][x]);
     }
   }
+}
 
-  state->rules[0][0][0] = false;
-  state->rules[0][0][1] = true;
-  state->rules[0][0][2] = false;
+void move_cells() {
+  for (int y = CELLSINHEIGHT - 1; y > 0; --y) {
+    for (int x = 0; x < CELLSINWIDTH; ++x) {
+      state->cell_states[y][x] = state->cell_states[y - 1][x];
+    }
+  }
+}
 
-  state->rules[0][1][0] = false;
-  state->rules[0][1][1] = false;
-  state->rules[0][1][2] = true;
+void parse_rule_file() {
+  FILE *file;
+  file =
+      fopen("/home/owner/projects/2d-cellular-automata-builder/rule.txt", "r");
+  if (!file) {
+    printf("Couldn't read file.");
+    exit(0);
+  }
 
-  state->rules[1][0][0] = true;
-  state->rules[1][0][1] = true;
-  state->rules[1][0][2] = true;
+  char c;
+  int size = 0;
+  while ((c = getc(file)) != EOF) {
+    if (c == '\n') {
+      size = 0;
+      continue;
+    }
+    if (size == 6) {
+      printf("Something went wrong while reading rules");
+      exit(1);
+    }
 
-  state->rules[1][1][0] = false;
-  state->rules[1][1][1] = true;
-  state->rules[1][1][2] = false;
+    if (size == 0)
+      state->rule_definitions[state->rules_size].rule[size] = c;
+
+    if (size == 1)
+      state->rule_definitions[state->rules_size].rule[size] = c;
+
+    if (size == 2)
+      state->rule_definitions[state->rules_size].rule[size] = c;
+
+    if (size == 3 || size == 4 || size == 5)
+      continue;
+
+    if (size == 6) {
+      state->rule_definitions[state->rules_size].center_outcome = c;
+      state->rules_size++;
+    }
+
+    printf("%c", c);
+
+    size++;
+  }
+
+  fclose(file);
 }
 
 void execute_rule() {
@@ -86,38 +130,27 @@ void execute_rule() {
     bool right = state->cell_states[1][x + 1];
 
     for (int j = 0; j < state->rules_size; ++j) {
-      bool left_rule = state->rules[j][0][0];
-      bool center_rule = state->rules[j][0][1];
-      bool right_rule = state->rules[j][0][2];
+      bool left_rule = state->rule_definitions[j].rule[0];
+      bool center_rule = state->rule_definitions[j].rule[1];
+      bool right_rule = state->rule_definitions[j].rule[2];
       if (left == left_rule && center == center_rule && right == right_rule) {
-        state->cell_states[0][x - 1] = state->rules[j][1][0];
-        state->cell_states[0][x] = state->rules[j][1][1];
-        state->cell_states[0][x + 1] = state->rules[j][1][2];
-
-        if (state->cell_states[0][x - 1]) {
-          SDL_SetRenderDrawColor(state->renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-        } else {
-          SDL_SetRenderDrawColor(state->renderer, 255, 255, 255,
-                                 SDL_ALPHA_OPAQUE);
-        }
-        SDL_RenderFillRect(state->renderer, state->cells[0][x - 1]);
-
-        if (state->cell_states[0][x]) {
-          SDL_SetRenderDrawColor(state->renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-        } else {
-          SDL_SetRenderDrawColor(state->renderer, 255, 255, 255,
-                                 SDL_ALPHA_OPAQUE);
-        }
-        SDL_RenderFillRect(state->renderer, state->cells[0][x]);
-
-        if (state->cell_states[0][x + 1]) {
-          SDL_SetRenderDrawColor(state->renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-        } else {
-          SDL_SetRenderDrawColor(state->renderer, 255, 255, 255,
-                                 SDL_ALPHA_OPAQUE);
-        }
-        SDL_RenderFillRect(state->renderer, state->cells[0][x + 1]);
+        state->cell_states[0][x] = state->rule_definitions[j].center_outcome;
       }
+    }
+  }
+}
+
+void render_state() {
+  for (int y = 0; y < CELLSINHEIGHT; ++y) {
+    for (int x = 0; x < CELLSINWIDTH; ++x) {
+      if (state->cell_states[y][x]) {
+        SDL_SetRenderDrawColor(state->renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+      } else {
+        SDL_SetRenderDrawColor(state->renderer, 255, 255, 255,
+                               SDL_ALPHA_OPAQUE);
+      }
+
+      SDL_RenderFillRect(state->renderer, state->cells[y][x]);
     }
   }
 
@@ -127,18 +160,20 @@ void execute_rule() {
 int main(void) {
   arena_t *arena = arena_create();
   if (!arena) {
-    printf("Failed to init memory arena");
     return 1;
   }
 
   init(arena);
+  parse_rule_file();
   bool running = 1;
+
   while (running) {
     SDL_Event event;
 
+    move_cells();
+    execute_rule();
+    render_state();
     while (SDL_PollEvent(&event)) {
-      execute_rule();
-      SDL_Delay(50);
       switch (event.type) {
       case SDL_QUIT:
         running = 0;
